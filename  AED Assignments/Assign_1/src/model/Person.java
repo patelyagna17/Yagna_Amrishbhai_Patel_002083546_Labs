4c7f67ff3ffc5076a26442aa69cb0cff87814fed
yagnapatel8 <patel.yagna@northeastern.edu>
2024-09-19 20:35:24 -0400

    Final Commit.

---
+++  AED Assignments/Assign_1/src/model/Person.java
@@ -0,0 +1,197 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+*
+ * @author yagnapatel
+ */
+public class Person {
+    String FirstName;
+    String LastName;
+    String DOB;
+    String Email;
+    String PhoneNumber;
+    String Gender;
+    String DriversLicenseNumber;
+    String DriversLicenseState;
+    String SocialSecurityNumber;
+    String AddressLine1;
+    String AddressLine2;
+    String City;
+    String State;
+    String Zip;
+    String TelephoneNumber;
+    String FaxNumber;
+    String CompanyName;
+    String CompanyEmail;
+    String CompanyPhone;
+    String CompanyAddress;
+
+    public String getFirstname() {
+        return FirstName;
+    }
+
+    public void setFirstname(String Firstname) {
+        this.FirstName = Firstname;
+    }
+
+    public String getLastname() {
+        return LastName;
+    }
+
+    public void setLastname(String Lastname) {
+        this.LastName = Lastname;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getDriversLicenseNumber() {
+        return DriversLicenseNumber;
+    }
+
+    public void setDriversLicenseNumber(String DriversLicenseNumber) {
+        this.DriversLicenseNumber = DriversLicenseNumber;
+    }
+
+    public String getDriversLicenseState() {
+        return DriversLicenseState;
+    }
+
+    public void setDriversLicenseState(String DriversLicenseState) {
+        this.DriversLicenseState = DriversLicenseState;
+    }
+
+    public String getSocialSecurityNumber() {
+        return SocialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String SocialSecurityNumber) {
+        this.SocialSecurityNumber = SocialSecurityNumber;
+    }
+
+    public String getAddressLine1() {
+        return AddressLine1;
+    }
+
+    public void setAddressLine1(String AddressLine1) {
+        this.AddressLine1 = AddressLine1;
+    }
+
+    public String getAddressLine2() {
+        return AddressLine2;
+    }
+
+    public void setAddressLine2(String AddressLine2) {
+        this.AddressLine2 = AddressLine2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZip() {
+        return Zip;
+    }
+
+    public void setZip(String Zip) {
+        this.Zip = Zip;
+    }
+
+    public String getTelephoneNumber() {
+        return TelephoneNumber;
+    }
+
+    public void setTelephoneNumber(String TelephoneNumber) {
+        this.TelephoneNumber = TelephoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return FaxNumber;
+    }
+
+    public void setFaxNumber(String FaxNumber) {
+        this.FaxNumber = FaxNumber;
+    }
+
+    public String getCompanyName() {
+        return CompanyName;
+    }
+
+    public void setCompanyName(String CompanyName) {
+        this.CompanyName = CompanyName;
+    }
+
+    public String getCompanyEmail() {
+        return CompanyEmail;
+    }
+
+    public void setCompanyEmail(String CompanyEmail) {
+        this.CompanyEmail = CompanyEmail;
+    }
+
+    public String getCompanyPhone() {
+        return CompanyPhone;
+    }
+
+    public void setCompanyPhone(String CompanyPhone) {
+        this.CompanyPhone = CompanyPhone;
+    }
+
+    public String getCompanyAddress() {
+        return CompanyAddress;
+    }
+
+    public void setCompanyAddress(String CompanyAddress) {
+        this.CompanyAddress = CompanyAddress;
+    }
+    
+   
+    
+    
+    
+}
